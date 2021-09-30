6e9fbe6a6a2f11e7dfee56499119b9b908d9e1fb
Avula Parvathi <avulaparvathi2000@gmail.com>
2021-09-30 12:05:01 -0700

    modified

---
+++ casestudy exception.py
@@ -0,0 +1,12 @@
+from casestudy import *
+def time(hours):
+    if hours<40:
+        raise Timesheet("less tha 40 hours")
+    else:
+        print("submit the timesheet")
+try:
+    time(20)
+except(Timesheet):
+    print("message")
+time(40)
+
